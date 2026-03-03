c360cde4da21a97037202f37aac6eba3b45b5ecc
Jakub Szczepaniak <autioch@gmail.com>
2026-03-03 14:06:02 +0100

    Update lesson plan

--- src/data/lessonTypes.ts
+++ src/data/lessonTypes.ts
@@ -1,35 +1,36 @@
 export type Teacher = {
-    id: string;
-    name: string;
-    email: string;
+  id: string;
+  name: string;
+  email: string;
 };
 
 export type Slot = {
-    start: string;
-    duration: number;
+  start: string;
+  duration: number;
 };
 
 export type LessonType = {
-    id: string;
-    name: string;
-    iconId: string;
-    color: string;
+  id: string;
+  name: string;
+  iconId: string;
+  color: string;
 };
 
 export type Lesson = {
-    lessonId: string;
-    teacherId: string;
-    paused: boolean;
+  lessonId: string;
+  teacherId: string;
+  paused: boolean;
+  ignored?: boolean;
 };
 
 export type Day = {
-    name: string;
-    lessons: Lesson[];
+  name: string;
+  lessons: Lesson[];
 };
 
 export type LessonsPlan = {
-    teachers: Teacher[];
-    slots: Slot[];
-    lessonTypes: LessonType[];
-    days: Day[];
+  teachers: Teacher[];
+  slots: Slot[];
+  lessonTypes: LessonType[];
+  days: Day[];
 };
